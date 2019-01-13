99a4901e41965746ecddd09926f282247ee3e6e0
deploy <kebron@bigmachines.io>
2019-01-13 22:08:12 +1000

    updated IP

--- section_4/code/cd_pipeline/docker_commands.sh
+++ section_4/code/cd_pipeline/docker_commands.sh
@@ -1,9 +1,10 @@
 # prefix commands with sudo if user is not in docker group
 # build docker image
-docker build -t hands-on-jenkins/myapp .
+docker build -t ibmcloud-jenkins/myapp .
+
 
 # check docker image
-docker images | grep hands-on-jenkins/myapp
+docker images | grep ibmcloud-jenkins/myapp
 
 # run docker container
-docker run -p 8888:5000 --name myapp hands-on-jenkins/myapp 
+docker run -p 8090:5000 --name myapp ibmcloud-jenkins/myapp 
